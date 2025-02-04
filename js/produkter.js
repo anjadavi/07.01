fa73a3e6e8a2441fa63643cecdb97e17a783b034
anjadavi <anda0004@stud.kea.dk>
2025-02-04 11:14:04 +0100

    tilføjer dynamisk site

--- js/produkter.js
+++ js/produkter.js
@@ -33,3 +33,33 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
         </aside>
         `
     })
+
+    
+    const productId = new URLSearchParams(window.location.search).get("id");
+const productcontainer = document.querySelector(".produkt-container");
+
+let overskrift = document.querySelector("h2")
+overskrift.innerHTML = mycategory;
+
+fetch(`https://kea-alt-del.dk/t7/api/products?category=${productId}`)
+  .then((response) => response.json())
+  .then((data) => showList(data));
+
+
+function showList(products) {
+  console.log(products);
+
+  const markup = products
+    .map(
+      (product) =>
+        `<article class="produkt-kort">
+                <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="zip-up">
+                <p><b>${product.productdisplayname}</b></p>
+                <p class="price">DKK ${product.price},-</p>
+                <a href="produkter.html" class="read-more">READ MORE</a>
+            </article>`
+    )
+    .join("");
+  console.log(markup);
+  productcontainer.innerHTML = markup;
+}
--- js/produktliste.js
+++ js/produktliste.js
@@ -1,24 +1,27 @@
+const mycategory = new URLSearchParams(window.location.search).get("category");
 const productcontainer = document.querySelector(".liste-grid-container");
-let markup = "";
 
-fetch(`https://kea-alt-del.dk/t7/api/products/`)
-    .then((response) => response.json())
-    .then((data) => showList(data));
-    
-    function showList(products) {
-        console.log(products);
-        products.map(product=>{
-    })
-    products.map((data) => {
-        markup += `<article class="produkt-kort">
-                <img src="https://kea-alt-del.dk/t7/images/webp/640/${data.id}.webp" alt="zip-up">
-                <p><b>${data.productdisplayname}</b></p>
-                <p class="price">DKK ${data.price},-</p>
+let overskrift = document.querySelector("h2")
+overskrift.innerHTML = mycategory;
+
+fetch(`https://kea-alt-del.dk/t7/api/products?category=${mycategory}`)
+  .then((response) => response.json())
+  .then((data) => showList(data));
+
+function showList(products) {
+  console.log(products);
+
+  const markup = products
+    .map(
+      (product) =>
+        `<article class="produkt-kort">
+                <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="zip-up">
+                <p><b>${product.productdisplayname}</b></p>
+                <p class="price">DKK ${product.price},-</p>
                 <a href="produkter.html" class="read-more">READ MORE</a>
-            </article>`;
-        })
+            </article>`
+    )
     .join("");
-    console.log(markup);
-    productcontainer.innerHTML = markup;
-        
-    }
+  console.log(markup);
+  productcontainer.innerHTML = markup;
+}
