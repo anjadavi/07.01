84c9267ae4e8f7d1bc9e213113855b24dcc382b6
anjadavi <anda0004@stud.kea.dk>
2025-02-06 17:09:03 +0100

    gjort alt dynamisk

--- js/produkter.js
+++ js/produkter.js
@@ -1,22 +1,23 @@
-let productId = 1528
-let productcontainer = document.querySelector(".produkt-container");
+let produktContainer = document.querySelector(".produkt-container");
+const myId = new URLSearchParams(window.location.search).get("id");
 
-fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
-    .then(response => response.json())
-    .then(data => {
-        productcontainer.innerHTML = `
-        <section class="produkt-image">
-            <img src="img/overdel.webp" alt="FERRARI BLACK ZIP-UP">
+fetch(`https://kea-alt-del.dk/t7/api/products/${myId}`)
+  .then((response) => response.json())
+  .then((data) => {
+    produktContainer.innerHTML = `
+    <section class="produkt-image">
+    <a href="index.html">Home</a> > <a href="produktliste.html?category=${data.category}">${data.category}</a> > ${data.productdisplayname}
+           <img src="https://kea-alt-del.dk/t7/images/webp/640/${data.id}.webp" alt="Billede af produkt">
         </section>
 
         <section class="produkt-info">
             <h1>Product Information</h1>
             <p><strong>Model name</strong><br>${data.productdisplayname}, ${data.variantname}</p>
             <p><strong>Color</strong><br>${data.basecolour}</p>
-            <p><strong>Inventory number</strong><br>${productId}</p>
+            <p><strong>Inventory number</strong><br>${data.id}</p>
             <br>
             <br>
-            <h1>${data.brandname}</h2>
+            <h1>${data.brandname}</h1>
             <p>${data.brandbio}</p>
         </section>
 
@@ -31,6 +32,7 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
             </select>
             <button class="add-to-basket">Add to basket</button>
         </aside>
-        `
-    })
+ 
+`;
+  });
 
--- js/produktliste.js
+++ js/produktliste.js
@@ -1,27 +1,29 @@
-const mycategory = new URLSearchParams(window.location.search).get("category");
-const productcontainer = document.querySelector(".liste-grid-container");
+const myCategory = new URLSearchParams(window.location.search).get("category");
 
-let overskrift = document.querySelector("h2")
-overskrift.innerHTML = mycategory;
+const produktContainer = document.querySelector(".liste-grid-container");
+let overskrift = document.querySelector("h2");
+overskrift.innerHTML = myCategory;
 
-fetch(`https://kea-alt-del.dk/t7/api/products?category=${mycategory}`)
+fetch(`https://kea-alt-del.dk/t7/api/products?category=${myCategory}`)
   .then((response) => response.json())
   .then((data) => showList(data));
 
-function showList(products) {
-  console.log(products);
-
-  const markup = products
+function showList(data) {
+  const markup = data
     .map(
-      (product) =>
-        `<article class="produkt-kort">
+      (product) => 
+        `<article class="produkt-kort ${product.soldout && "sold-out"}">
                 <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="zip-up">
+                ${product.soldout > 0 ? `<span class="overlay">SOLD OUT</span>` : ""}
                 <p><b>${product.productdisplayname}</b></p>
+                ${product.discount > 0 ? `<p class="old-price"> DKK ${product.price} </p>` : ""}
+                ${product.discount > 0 ? `<p class="new-price">NU DKK ${product.price - product.discount},- <span class="discount">-${product.discount},-</span></p>`: ""}
                 <p class="price">DKK ${product.price},-</p>
-                <a href="produkter.html" class="read-more">READ MORE</a>
-            </article>`
+                <a href="produkter.html?id=${product.id}" class="read-more">READ MORE</a>
+            </article>
+        `
     )
+
     .join("");
-  console.log(markup);
-  productcontainer.innerHTML = markup;
+  produktContainer.innerHTML = markup;
 }
